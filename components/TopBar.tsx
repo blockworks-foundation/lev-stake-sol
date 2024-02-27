8c2b7009d72387f5ce0099722038266d942817b9
saml33 <slam.uke@gmail.com>
2024-02-27 20:31:34 +1100

    mobile nav

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,5 +1,9 @@
-import { useEffect, useState } from 'react'
-import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
+import { Fragment, useEffect, useState } from 'react'
+import {
+  Bars3Icon,
+  ExclamationTriangleIcon,
+  XMarkIcon,
+} from '@heroicons/react/20/solid'
 import { useWallet } from '@solana/wallet-adapter-react'
 import ConnectedMenu from './wallet/ConnectedMenu'
 import ConnectWalletButton from './wallet/ConnectWalletButton'
@@ -8,10 +12,13 @@ import ThemeToggle from './ThemeToggle'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import BoostLogo from './BoostLogo'
+import { IconButton } from './shared/Button'
+import { Transition } from '@headlessui/react'
+import { useViewport } from 'hooks/useViewport'
 
 const TopBar = () => {
   const { connected } = useWallet()
-
+  const { isMobile } = useViewport()
   const [copied, setCopied] = useState('')
   const isOnline = useOnlineStatus()
   const router = useRouter()
@@ -39,13 +46,21 @@ const TopBar = () => {
         </Link>
       </div>
       <div className="col-span-1 flex items-center justify-center space-x-4">
-        <NavLink active={pathname === '/'} path="/" text="Boost!" />
-        <NavLink active={pathname === '/stats'} path="/stats" text="Stats" />
-        <NavLink active={pathname === '/faqs'} path="/faqs" text="FAQs" />
+        {!isMobile ? (
+          <>
+            <NavLink active={pathname === '/'} path="/" text="Boost!" />
+            <NavLink
+              active={pathname === '/stats'}
+              path="/stats"
+              text="Stats"
+            />
+            <NavLink active={pathname === '/faqs'} path="/faqs" text="FAQs" />
+          </>
+        ) : null}
       </div>
       <div className="col-span-1 flex items-center justify-end">
         <div className="flex space-x-3">
-          <ThemeToggle />
+          {isMobile ? <MobileNavigation /> : <ThemeToggle />}
           {connected ? <ConnectedMenu /> : <ConnectWalletButton />}
         </div>
       </div>
@@ -62,6 +77,92 @@ const TopBar = () => {
 }
 
 export default TopBar
+
+const MobileNavigation = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  return (
+    <div className="lg:hidden">
+      <div className="flex items-center space-x-2">
+        <IconButton onClick={() => setShowMenu(true)} size="large">
+          <Bars3Icon className="h-5 w-5" />
+        </IconButton>
+      </div>
+      <MenuPanel showMenu={showMenu} setShowMenu={setShowMenu} />
+    </div>
+  )
+}
+
+const MenuPanel = ({
+  showMenu,
+  setShowMenu,
+}: {
+  showMenu: boolean
+  setShowMenu: (showMenu: boolean) => void
+}) => {
+  const closeOnClick = () => {
+    setShowMenu(false)
+  }
+
+  return (
+    <>
+      <div
+        className={`fixed right-0 top-0 z-40 h-screen w-[90%] overflow-hidden bg-th-bkg-2 transition duration-300 ease-in-out ${
+          showMenu ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className="flex justify-end px-6 py-3">
+          <IconButton onClick={() => setShowMenu(false)} hideBg size="medium">
+            <XMarkIcon className="h-8 w-8" />
+          </IconButton>
+        </div>
+        <div className="space-y-4 px-6">
+          <Link
+            className="block text-xl font-bold text-th-fgd-1"
+            href="/"
+            onClick={closeOnClick}
+            shallow
+          >
+            Boost!
+          </Link>
+          <Link
+            className="block text-xl font-bold text-th-fgd-1"
+            href="/stats"
+            onClick={closeOnClick}
+            shallow
+          >
+            Stats
+          </Link>
+          <Link
+            className="block text-xl font-bold text-th-fgd-1"
+            href="/faqs"
+            onClick={closeOnClick}
+            shallow
+          >
+            FAQs
+          </Link>
+        </div>
+        <div className="absolute bottom-8 right-8">
+          <ThemeToggle />
+        </div>
+      </div>
+      <Transition
+        as={Fragment}
+        show={showMenu}
+        enter="transition-all ease-in duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-all ease-out duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div
+          className={`fixed right-0 top-0 z-30 h-screen w-full backdrop-brightness-[0.3]`}
+          aria-hidden="true"
+        />
+      </Transition>
+    </>
+  )
+}
 
 const NavLink = ({
   active,
