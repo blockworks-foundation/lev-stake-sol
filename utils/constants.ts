07013ea3490194729a1149751ee1eec038f00b5e
Lou-Kamades <128186011+Lou-Kamades@users.noreply.github.com>
2024-03-19 20:58:14 +0100

    always supply usdc fallback oracle (#22)

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,5 @@
+import { PublicKey } from '@solana/web3.js'
+
 // lev stake
 export const BORROW_TOKEN = 'USDC'
 
@@ -188,3 +190,7 @@ export const MAX_ACCOUNTS = {
 
 export const ACCOUNT_ACTIONS_NUMBER_FORMAT_CLASSES =
   'w-full rounded-lg rounded-l-none border border-th-input-border bg-th-input-bkg p-3 text-right font-mono text-xl text-th-fgd-1 focus-visible:border-th-fgd-4 focus:outline-none md:hover:border-th-input-border-hover md:hover:focus-visible:border-th-fgd-4'
+
+export const FALLBACK_ORACLES = [
+  new PublicKey('Gnt27xtC473ZT2Mw5u8wZ68Z3gULkSTb5DuxJy7eJotD'),
+] // USDC
