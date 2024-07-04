ade43556ea2ff6fd8e86da022f458aeae0937a1f
Adrian Brzeziński <a.brzezinski94@gmail.com>
2024-07-04 22:41:03 +0200

    fix

--- components/notifications/TransactionNotification.tsx
+++ components/notifications/TransactionNotification.tsx
@@ -112,13 +112,13 @@ const TransactionNotificationList = () => {
         </button>
       ) : null}
       {reversedNotifications.map((n) => (
-        <TransactionNotification key={n.id} notification={n} />
+        <TransactionNotificationComponent key={n.id} notification={n} />
       ))}
     </div>
   )
 }
 
-const TransactionNotification = ({
+const TransactionNotificationComponent = ({
   notification,
 }: {
   notification: TransactionNotification
