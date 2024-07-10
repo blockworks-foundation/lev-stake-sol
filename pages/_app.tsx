283032dfae86d53238be214f5f9654d74899203d
saml33 <slam.uke@gmail.com>
2024-07-10 16:05:14 +1000

    update meta images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,7 +46,7 @@ import PlausibleProvider from 'next-plausible'
 // init react-query
 export const queryClient = new QueryClient()
 
-const metaTitle = 'Welcome Yield Fan'
+const metaTitle = 'For Fans of Yield'
 const metaDescription =
   'Earn leveraged yield on JLP and Liquid Staking Tokens. Powered by Mango'
 
@@ -101,7 +101,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome Yield Fan</title>
+        <title>For Fans of Yield | Yield Fan</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content={metaTitle} />
         <meta name="description" content={metaDescription} />
@@ -116,10 +116,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metaTitle} />
         <meta name="twitter:description" content={metaDescription} />
-        {/* <meta
+        <meta
           name="twitter:image"
-          content="https://boost.mango.markets/images/1200x675-share.png?345678790"
-        /> */}
+          content="https://yield.fan/images/1200x675-share.png?445678790"
+        />
         <meta name="google" content="notranslate" />
         <link rel="manifest" href="/manifest.json"></link>
       </Head>
